c2099bc553f907ee1fd0df5ec07e00e1db6c37f6
David Alderliesten <david.j.w@hotmail.com>
2020-04-11 17:46:42 +0200

    Add Timeout Guard
    Adding a timeout guard that was added to allow larger repositories, such
    as TensorFlow, to be analyzed without strange timeouts with error
    handling. Also prepared the code for release/public showing.

--- Code/good_first_issue.py
+++ Code/good_first_issue.py
@@ -1,6 +1,8 @@
 from tagged_issues import TaggedIssues
 from user_commits import UserCommits
 
+import socket
+
 
 class GoodFirstIssue:
     """Handles main application logic and does all tasks.
@@ -21,21 +23,25 @@ class GoodFirstIssue:
         Requires the manual entry of Github API key on line 24, repository link as described above the declaration on
         line 28, and the manual entry of the desired tag to analyze on line 31.
         """
-        # Provide the Github API key to utilize.
-        api_key = "34b8204b93e9ac470e5f34f55fe3f780a1a71c45"
+        try:
+            # Provide the Github API key to utilize.
+            api_key = "ADD API KEY"
 
-        # Define location of the repository. This should be an https:// link to Github with the .git still on it. This
-        # can be obtained by going to a repository's site and selecting the 'clone or download -> https' option.
-        repository = "https://github.com/w3c/csswg-drafts.git"
+            # Define location of the repository. This should be an https:// link to Github with the .git still on it.
+            # This can be obtained by going to a repository's site and selecting the 'clone or download -> https'
+            # option.
+            repository = "ADD REPO .git LOCATION"
 
-        # Define the name of the good-first-issue tag used in the repository to analyze.
-        first_issue_tag = "good first issue"
+            # Define the name of the good-first-issue tag used in the repository to analyze.
+            first_issue_tag = "good first issue"
 
-        # Get first commits for each user in the repository.
-        user_commits = UserCommits.get_first_commits(repository, api_key)
+            # Get first commits for each user in the repository.
+            user_commits = UserCommits.get_first_commits(repository, api_key)
 
-        # Get all tagged issues within the repository.
-        tagged_issues = TaggedIssues.get_tagged_issues(repository, api_key, first_issue_tag)
+            # Get all tagged issues within the repository.
+            tagged_issues = TaggedIssues.get_tagged_issues(repository, api_key, first_issue_tag)
+        except socket.timeout:
+            print('Timeout took place, repository should be re-run. Catch hopefully saves part of data.')
 
 
 # Main method call for program execution.
